0b54e65da708195a8aedbfa3495c6df28bf804fd
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-15 11:29:51 +0530

    added array, onsubmit , debug message

--- src/App.svelte
+++ src/App.svelte
@@ -8,20 +8,36 @@ class Variable {
 		this.value = value;
 	}
 }
+let variableArray = [];
+let variableCount = 0;
 let  v1  = new Variable('v1', '');
+function handleOnSubmit(){
+	console.log("inside handleOnSubmit");
+	variableArray.push(v1);
+	variableName = "v"+ variableCounter;
+	variableCounter++;
+	v1 = new Variable(variableName, '');
+}
 </script>
 
 <main>
 	<h1>Variable</h1>
 	<p>Add variable , get variable, edit variable, delete variable , list, search </p>
 	<h3>Add Variable</h3>
-	<form>
+	<form on:submit={handleOnSubmit}>
 		<label for="name">Name:</label><br>
 		<input type="text" id="variableName" name="variableName" bind:value={v1.name}><br>
 		<label for="value">Value:</label><br>
 		<input type="text" id="variableValue" name="variableValue" bind:value={v1.value}><br>
       	<input type="submit" value="Submit">
 	</form>
+
+	<h3>
+		Debugging 
+	</h3>
+	<hr/>
+
+	<p>Variable Array: {JSON.stringify(variableArray)} </p>
 </main>
 
 <style>
