e0a086caf08b1928ec91a516e7d84a7dea1b743d
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-15 11:21:58 +0530

    variable class

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,13 @@
 let name = 'Salma';
 let variableName = '';
 let variableValue = '';
+class Variable {
+	constructor(name, value){
+		this.name = name;
+		this.value = value;
+	}
+}
+let  v1  = new Variable('v1', '');
 </script>
 
 <main>
@@ -10,9 +17,9 @@ let variableValue = '';
 	<h3>Add Variable</h3>
 	<form>
 		<label for="name">Name:</label><br>
-		<input type="text" id="variableName" name="variableName" bind:value={variableName}><br>
+		<input type="text" id="variableName" name="variableName" bind:value={v1.name}><br>
 		<label for="value">Value:</label><br>
-		<input type="text" id="variableValue" name="variableValue" bind:value={variableValue}><br>
+		<input type="text" id="variableValue" name="variableValue" bind:value={v1.value}><br>
       	<input type="submit" value="Submit">
 	</form>
 </main>
