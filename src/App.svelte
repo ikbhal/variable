7ab9ec972164b0dcb4986bea822081a861fd514a
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-15 11:00:42 +0530

    add variable form

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,15 @@
 <main>
 	<h1>Variable</h1>
 	<p>Add variable , get variable, edit variable, delete variable , list, search </p>
+	<h3>Add Variable</h3>
+	<form>
+		<label for="name">Name:</label><br>
+		<input type="text" id="fname" name="fname"><br>
+		<label for="value">Value:</label><br>
+		<input type="text" id="lname" name="lname"><br>
+      	<input type="submit" value="Submit">
+	</form>
+
 </main>
 
 <style>
